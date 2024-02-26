1ecfca29436b4399271e66237dbd0e322f27be19
Sebastian Galeano <sebascarp9@gmail.com>
2024-02-26 17:38:12 -0300

    Formulario de contacto

--- src/Pages/Contacto.jsx
+++ src/Pages/Contacto.jsx
@@ -1,9 +1,21 @@
+import './Contacto.css'
+
 export function Contacto() {
   return (
-    <>
-    <div>
-        
-    </div>
-    </>
+    <section>
+          <form action="">
+            <label htmlFor="Nombre">Nombre: </label>
+            <input type="text" placeholder='Ingrese su nombre' />
+            <label htmlFor="Apellido">Apellido: </label>
+            <input type="text" name="Apellido" placeholder='Ingrese su apellido' />
+            <label htmlFor="Email">Email: </label>
+            <input type="email" placeholder='Ingrese un email' />
+            <label htmlFor="Telefono">Tel: </label>
+            <input type="tel" placeholder='Ingrese su numero' />
+            <label htmlFor="Mensaje">Mensaje: </label>
+            <textarea name="Mensaje" id="Mensaje" cols="30" rows="10"></textarea>
+            <button>Enviar  </button>
+          </form>
+        </section>
   );
 }
--- src/Pages/Productos.jsx
+++ src/Pages/Productos.jsx
@@ -1,5 +1,6 @@
-import { ProductCard } from "../component/Nav/ProductCard/ProductCard";
+import { ProductCard } from "../component/ProductCard/ProductCard";
 import { productos } from "../db/productos.json";
+
 export function Productos() {
   return <ProductCard productos={productos} />;
 }
