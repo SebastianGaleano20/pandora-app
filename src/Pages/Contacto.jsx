038cae961a0716ffd9c245380b3d7377763639e7
Sebastian Galeano <sebascarp9@gmail.com>
2024-02-26 19:00:38 -0300

    Arreglos responsive

--- src/Pages/Contacto.jsx
+++ src/Pages/Contacto.jsx
@@ -1,21 +1,23 @@
-import './Contacto.css'
+import "./Contacto.css";
 
 export function Contacto() {
   return (
-    <section>
-          <form action="">
-            <label htmlFor="Nombre">Nombre: </label>
-            <input type="text" placeholder='Ingrese su nombre' />
-            <label htmlFor="Apellido">Apellido: </label>
-            <input type="text" name="Apellido" placeholder='Ingrese su apellido' />
-            <label htmlFor="Email">Email: </label>
-            <input type="email" placeholder='Ingrese un email' />
-            <label htmlFor="Telefono">Tel: </label>
-            <input type="tel" placeholder='Ingrese su numero' />
-            <label htmlFor="Mensaje">Mensaje: </label>
-            <textarea name="Mensaje" id="Mensaje" cols="30" rows="10"></textarea>
-            <button>Enviar  </button>
-          </form>
-        </section>
+    <div className="cont-formu">
+      <form action="#">
+        
+      <h1>Contactate con nosotros! </h1> 
+        <label htmlFor="Nombre">Nombre: </label>
+        <input type="text" placeholder="Ingrese su nombre" />
+        <label htmlFor="Apellido">Apellido: </label>
+        <input type="text" name="Apellido" placeholder="Ingrese su apellido" />
+        <label htmlFor="Email">Email: </label>
+        <input type="email" placeholder="Ingrese un email" />
+        <label htmlFor="Telefono">Tel: </label>
+        <input type="tel" placeholder="Ingrese su numero" />
+        <label htmlFor="Mensaje">Mensaje: </label>
+        <textarea name="Mensaje" id="Mensaje" cols="30" rows="10"></textarea>
+        <button>Enviar </button>
+      </form>
+    </div>
   );
 }
