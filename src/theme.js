d179a7a1c9fb4393599257cee287be0e98560948
Sebastian Galeano <sebascarp9@gmail.com>
2024-08-18 13:24:45 -0300

    mui added

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#eb5e28',
+    },
+    secondary: {
+      main: '#403d39',
+      light: '#fffcf2',
+      dark:'#252422' ,
+      contrastText: 'ccc5b9',
+    },
+  },
+});
